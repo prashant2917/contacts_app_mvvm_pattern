4e31697a8ee3e9e58608a9a1fcc8910d9ac0764b
Prashant Jondhale <jondhaleprashant@gmail.com>
2021-03-08 22:49:36 +0530

    added contact class

---
+++ app/src/main/java/com/pocket/kumbhashree/model/Contact.kt
@@ -0,0 +1,13 @@
+package com.pocket.kumbhashree.model
+
+class Contact(
+    val id: Int,
+    val firstName: String? = null,
+    val middleName: String? = null,
+    val lastName: String? = null,
+    val address: String? = null,
+    val occupation: String? = null,
+    val mobileNo1: String? = null,
+    val mobileNo2: String? = null,
+    val profileImageUrl: String? = null,
+)
