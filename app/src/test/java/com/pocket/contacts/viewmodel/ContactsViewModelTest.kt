c7c2b1b0cbc3d4a05af4843573c97f49a050e08f
Prashant Jondhale <jondhaleprashant@gmail.com>
2021-07-18 11:37:09 +0530

    Add AddContactSuccess Test

--- app/src/test/java/com/pocket/contacts/viewmodel/ContactsViewModelTest.kt
+++ app/src/test/java/com/pocket/contacts/viewmodel/ContactsViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.pocket.contacts.viewmodel
 
 import com.pocket.contacts.model.ContactModel
+import com.pocket.contacts.model.ResponseModel
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,12 +19,25 @@ class ContactsViewModelTest {
         val contactModel = ContactModel()
         assertTrue(contactModel.count == 0)
     }
+    @Test
+    fun addContactSuccess() {
+        val responseModel = getResponseModel()
+        assertTrue(responseModel.status == 1)
+    }
 
     private fun getContactModel(): ContactModel {
         var contactModel = ContactModel()
         return contactModel.apply {
             this.count = 10
             this.status = "ok"
+        }
+    }
+
+    private fun getResponseModel(): ResponseModel {
+        var responseModel = ResponseModel()
+        return responseModel.apply {
+            this.status = 1
+            this.message = "Contact Added Successfully"
         }
     }
 }
